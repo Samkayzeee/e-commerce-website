da561bed4b8171dc8877409d0dc1e144774549ae
Samkayzeee <lasisiabdulsamad9@gmail.com>
2023-03-21 23:13:49 +0100

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,19 @@
-
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return ( 
         <>
             <footer className="footer">
-                footer
+                <div className="first">
+                    SAMKAYZEE E-COMMERCE-WEBSITE All right reserved - Developed by Lasisi Abdulsamad
+                </div>
+
+                <div className="icons">
+                        <li> <Link to={'https://github.com/Samkayzeee'}><i class="fa-brands fa-github"></i></Link> </li>
+                        <li> <Link to={'https://twitter.com/Samkayzee1'}><i class="fa-brands fa-twitter"></i></Link> </li>
+                        <li> <Link to={'https://www.linkedin.com/in/lasisi-abdulsamad/'}><i class="fa-brands fa-linkedin"></i></Link> </li>
+                </div>
             </footer>
         </>
      );
--- src/pages/carts/Carts.jsx
+++ src/pages/carts/Carts.jsx
@@ -1,8 +1,19 @@
 import DefaultLayout from "../../layouts/DefaultLayouts/DefaultLayout";
 import './Carts.css';
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 
 const Cart = () => {
+   const navigate = useNavigate();
+    let token = localStorage.getItem("token");
+
+    useEffect(() => {
+      if (!token) {
+         navigate('/login');
+     }
+    },[])
+
     return ( 
         <>
            <div className="cart">
