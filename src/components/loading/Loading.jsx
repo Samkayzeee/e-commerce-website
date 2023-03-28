75130601427743eff192c3992949e23a6c87b030
Samkayzeee <lasisiabdulsamad9@gmail.com>
2023-03-28 23:13:58 +0100

    changed the pages loading

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -3,7 +3,11 @@ import './Loading.css';
 const Loading = () => {
     return ( 
         <>
-            <h1 className='Loading'>Loading...</h1>
+           <div className="loading">
+                <div class="loadingio-spinner-eclipse-1hjaczvx3dy"><div class="ldio-gckd814k9uq">
+                <div></div>
+                </div></div>
+            </div>
         </>
      );
 }
